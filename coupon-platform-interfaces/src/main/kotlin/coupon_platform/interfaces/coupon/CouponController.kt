423c49f1f1eaab61df98117a8ec6400e960ce544
jihoon <znftm97@naver.com>
2023-09-01 19:46:14 +0900

    [#1] fix : 불필요한 facade 제거

--- coupon-platform-interfaces/src/main/kotlin/coupon_platform/interfaces/coupon/CouponController.kt
+++ coupon-platform-interfaces/src/main/kotlin/coupon_platform/interfaces/coupon/CouponController.kt
@@ -1,6 +1,6 @@
 package coupon_platform.interfaces.coupon
 
-import coupon_platform.domain.coupon.facade.CouponFacade
+import coupon_platform.domain.coupon.service.CouponService
 import coupon_platform.interfaces.common.response.BaseResponse
 import coupon_platform.interfaces.coupon.dto.CreateCouponRequest
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/coupons")
 class CouponController(
-    val couponFacade: CouponFacade,
+    val couponService: CouponService,
 ) {
 
     @PostMapping
     fun createCoupon(createCouponRequest: CreateCouponRequest): BaseResponse<String> {
-        val couponName = couponFacade.createCoupon(createCouponRequest.toCommand())
+        val couponName = couponService.createCoupon(createCouponRequest.toCommand())
         return BaseResponse.success(couponName)
     }
 

b671d49eab05f646430cf0716afa8d8f952fca91
jihoon <znftm97@naver.com>
2023-09-06 21:03:33 +0900

    [#3] fix : application 모듈 추가

--- coupon-platform-interfaces/build.gradle.kts
+++ coupon-platform-interfaces/build.gradle.kts
@@ -1,7 +1,8 @@
 dependencies {
+    implementation(project(":coupon-platform-application"))
     implementation(project(":coupon-platform-domain"))
     implementation(project(":coupon-platform-infrastructure"))
-    
+
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,7 @@
 rootProject.name = "coupon_platform"
-include("coupon-platform-domain", "coupon-platform-infrastructure", "coupon-platform-interfaces")
+include(
+    "coupon-platform-domain",
+    "coupon-platform-infrastructure",
+    "coupon-platform-interfaces",
+    "coupon-platform-application"
+)

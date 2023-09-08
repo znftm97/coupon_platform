c000740f2e62c45fd1425b4d389f35c271f7db1e
jihoon <znftm97@naver.com>
2023-09-08 21:52:42 +0900

    [#7] feat : 쿠폰 코드 생성 플로우 정의(Facade 생성)

---
+++ coupon-platform-application/src/main/kotlin/coupon_platform/application/coupon_code/CouponCodeFacade.kt
@@ -0,0 +1,20 @@
+package coupon_platform.application.coupon_code
+
+import coupon_platform.domain.coupon.service.CouponReadService
+import coupon_platform.domain.coupon_code.dto.CouponCodeCreateCommand
+import coupon_platform.domain.coupon_code.service.CouponCodeStoreService
+import org.springframework.stereotype.Service
+
+@Service
+class CouponCodeFacade(
+    val couponCodeStoreService: CouponCodeStoreService,
+    val couponReadService: CouponReadService,
+) {
+    fun createCouponCode(couponCodeCreateCommand: CouponCodeCreateCommand): String {
+        // 존재하는 쿠폰인지 확인하기 위해 조회
+        couponReadService.findCouponById(couponCodeCreateCommand.couponId)
+        val couponCodeInfo = couponCodeStoreService.createCouponCode(couponCodeCreateCommand)
+
+        return couponCodeInfo.couponCode
+    }
+}

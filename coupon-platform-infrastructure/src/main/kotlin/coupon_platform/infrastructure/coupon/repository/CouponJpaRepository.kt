7160ed8a7a1e5d6d09690b9b421930b62e46fd41
jihoon <znftm97@naver.com>
2023-09-08 18:30:44 +0900

    [#7] feat : 쿠폰 코드로 쿠폰 발급 저장 로직 구현

--- coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/coupon/repository/CouponJpaRepository.kt
+++ coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/coupon/repository/CouponJpaRepository.kt
@@ -2,7 +2,12 @@ package coupon_platform.infrastructure.coupon.repository
 
 import coupon_platform.domain.coupon.entity.Coupon
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface CouponJpaRepository : JpaRepository<Coupon, Long> {
-    fun findByName(name: String): Coupon
+    fun findByName(name: String): Coupon?
+
+    @Query("select c from Coupon c where c.id = :id")
+    fun customFindById(@Param("id") id: Long): Coupon?
 }
--- coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/coupon/repository/CouponReaderImpl.kt
+++ coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/coupon/repository/CouponReaderImpl.kt
@@ -1,5 +1,6 @@
 package coupon_platform.infrastructure.coupon.repository
 
+import coupon_platform.domain.common.exception.coupon.NotFoundCouponException
 import coupon_platform.domain.coupon.entity.Coupon
 import coupon_platform.domain.coupon.repository.CouponReader
 import org.springframework.stereotype.Component
@@ -9,6 +10,12 @@ class CouponReaderImpl(
     val couponJpaRepository: CouponJpaRepository,
 ) : CouponReader {
 
-    override fun readCoupon(name: String): Coupon = couponJpaRepository.findByName(name)
+    override fun findCouponByName(name: String): Coupon {
+        return couponJpaRepository.findByName(name) ?: throw NotFoundCouponException()
+    }
+
+    override fun findCouponById(id: Long): Coupon {
+        return couponJpaRepository.customFindById(id) ?: throw NotFoundCouponException()
+    }
 
 }

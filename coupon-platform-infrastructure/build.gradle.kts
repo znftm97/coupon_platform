4293c6e231b8873eabb8ef54878db9007dac9007
jihoon <znftm97@naver.com>
2023-09-12 13:38:02 +0900

    [#9] dependency : coroutine 사용을 위한 라이브러리 추가

--- coupon-platform-infrastructure/build.gradle.kts
+++ coupon-platform-infrastructure/build.gradle.kts
@@ -2,8 +2,10 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
     implementation(project(":coupon-platform-domain"))
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.github.f4b6a3:tsid-creator:5.2.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
 
 tasks.register("prepareKotlinBuildScriptModel") {}

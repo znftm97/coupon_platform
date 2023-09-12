622a2a8f1ce3406cb515219db16acc0b253cff65
jihoon <znftm97@naver.com>
2023-09-12 13:39:19 +0900

    [#9] feat : 코루틴을 사용하는 난수 생성 인터페이스 분리 및 구현

--- coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/coupon_code/random_number_generator/TSIDGenerator.kt
+++ coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/coupon_code/random_number_generator/TSIDGenerator.kt
@@ -1,36 +1,55 @@
 package coupon_platform.infrastructure.coupon_code.random_number_generator
 
+import com.github.f4b6a3.tsid.Tsid
 import com.github.f4b6a3.tsid.TsidCreator
 import coupon_platform.domain.common.exception.InvalidRandomNumberLengthException
-import coupon_platform.domain.coupon_code.RandomNumberGenerator
+import coupon_platform.domain.coupon_code.SuspendableRandomNumberGenerator
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
 @Component
 @Qualifier("TSIDGenerator")
-class TSIDGenerator() : RandomNumberGenerator {
+class TSIDGenerator() : SuspendableRandomNumberGenerator {
 
     /**
      * @param length 범위 -> 0 < length <= 13 (TSID는 최대길이가 13 이다.)
      */
-    override fun generate(length: Int): String {
+    override suspend fun generate(length: Int): String {
         require(length in 13 downTo 1) {
             throw InvalidRandomNumberLengthException()
         }
 
-        return TsidCreator.getTsid().toString().substring(0 until length)
+        val tsid: Tsid
+        coroutineScope {
+            val job = async {
+                TsidCreator.getTsid()
+            }
+            tsid = job.await()
+        }
+
+        return tsid.toString().substring(0 until length)
     }
 
     /**
      * @param length 범위 -> 0 < length <= 13
      * @param prefix 범위 -> 0 <= prefix <= 10
      */
-    override fun generateWithPrefix(length: Int, prefix: String): String {
+    override suspend fun generateWithPrefix(length: Int, prefix: String): String {
         require(length in 13 downTo 1 && prefix.length in 10 downTo 0) {
             throw InvalidRandomNumberLengthException()
         }
 
-        return prefix.plus(TsidCreator.getTsid().toString()).substring(0 until length)
+        val tsid: Tsid
+        coroutineScope {
+            val job = async {
+                TsidCreator.getTsid()
+            }
+            tsid = job.await()
+        }
+
+        return prefix.plus(tsid.toString()).substring(0 until length)
     }
 
 }

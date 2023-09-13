4b8283a912df9cffe641516ae7a2842ee16e00d5
jihoon <znftm97@naver.com>
2023-09-13 22:27:13 +0900

    [#12] refactor : 쿠폰 상태 속성 추가

--- coupon-platform-domain/src/main/kotlin/coupon_platform/domain/coupon/entity/Coupon.kt
+++ coupon-platform-domain/src/main/kotlin/coupon_platform/domain/coupon/entity/Coupon.kt
@@ -14,6 +14,9 @@ class Coupon private constructor(
     @Enumerated(value = EnumType.STRING)
     val discountType: DiscountType,
 
+    @Enumerated(value = EnumType.STRING)
+    val status: CouponStatus,
+
     val externalId: String = "",
 
     @Id
@@ -29,7 +32,7 @@ class Coupon private constructor(
             applyType: ApplyType,
             discountType: DiscountType,
             externalId: String,
-        ) = Coupon(name, applyType, discountType, externalId)
+        ) = Coupon(name, applyType, discountType, CouponStatus.CREATED, externalId)
     }
 
     enum class DiscountType(description: String) {
@@ -40,6 +43,13 @@ class Coupon private constructor(
     enum class ApplyType(description: String) {
         PRODUCT("상품"),
         ACCOUNT("사용자")
+    }
+
+    enum class CouponStatus(description: String) {
+        CREATED("생성됨"),
+        ISSUED("발급됨"),
+        USED("사용됨"),
+        EXPIRED("만료됨"),
     }
 }
 

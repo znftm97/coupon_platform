8d7b62ec7ab28bb7b0b113a7b3e8308ec95df7ae
jihoon <znftm97@naver.com>
2023-09-16 21:25:43 +0900

    [#12] test : 테스트에 필요한 더미데이터 추가 및 삭제 sql

---
+++ coupon-platform-infrastructure/src/test/resources/data/clean-up.sql
@@ -0,0 +1,20 @@
+delete
+from account;
+alter table account
+    auto_increment = 1;
+delete
+from coupon;
+alter table coupon
+    auto_increment = 1;
+delete
+from coupon_code;
+alter table coupon_code
+    auto_increment = 1;
+delete
+from issued_coupon;
+alter table issued_coupon
+    auto_increment = 1;
+delete
+from product;
+alter table product
+    auto_increment = 1;

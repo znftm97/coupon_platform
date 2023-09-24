25a922f474e118ca030f5bff585591a6591665d4
jihoon <znftm97@naver.com>
2023-09-24 19:28:34 +0900

    [#14] dependency : redis 관련 의존성 추가, infrastructure 모듈 의존성 추가(빈 등록 이슈)

--- coupon-platform-batch/build.gradle.kts
+++ coupon-platform-batch/build.gradle.kts
@@ -3,9 +3,13 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 dependencies {
     implementation(project(":coupon-platform-domain"))
     implementation(project(":coupon-platform-application"))
+    implementation(project(":coupon-platform-infrastructure"))
 
     implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.redisson:redisson-spring-boot-starter:3.23.2")
+    implementation("io.lettuce:lettuce-core:6.2.6.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.1.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
     testImplementation("org.springframework.batch:spring-batch-test")
 }
 

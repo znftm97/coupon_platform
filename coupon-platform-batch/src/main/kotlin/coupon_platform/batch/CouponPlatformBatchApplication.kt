32605cefe3423c06548c50ee40a94c02b806fd76
jihoon <znftm97@naver.com>
2023-09-24 20:02:45 +0900

    [#14] config : 배치모듈 - 설정파일 추가, 빈 스캔 범위 설정

--- coupon-platform-batch/src/main/kotlin/coupon_platform/batch/CouponPlatformBatchApplication.kt
+++ coupon-platform-batch/src/main/kotlin/coupon_platform/batch/CouponPlatformBatchApplication.kt
@@ -2,8 +2,10 @@ package coupon_platform.batch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["coupon_platform"])
 class CouponPlatformBatchApplication
 
 fun main(args: Array<String>) {

de7033f97bc72802bb2fe8e17d9e0b8ad6e14b19
jihoon <znftm97@naver.com>
2023-09-24 16:32:12 +0900

    [#14] fix : 최초 출석체크(로그인)시 비트1 설정 누락 수정, lettuce 변경으로 인한 코드 수정

--- coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/account/AccountStoreImpl.kt
+++ coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/account/AccountStoreImpl.kt
@@ -2,7 +2,7 @@ package coupon_platform.infrastructure.account
 
 import coupon_platform.domain.account.AccountStore
 import coupon_platform.domain.common.CommonConstants.MAX_ACCOUNTS_NUMBER
-import org.redisson.api.RedissonClient
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -10,7 +10,7 @@ import java.util.*
 
 @Component
 class AccountStoreImpl(
-    val redissonClient: RedissonClient
+    val redisTemplate: RedisTemplate<String, BitSet>
 ) : AccountStore {
 
     companion object {
@@ -19,11 +19,16 @@ class AccountStoreImpl(
 
     override fun attendance(userId: Long) {
         val key = generateKey()
-        val bitSet = redissonClient.getBitSet(key)
+        val redis = redisTemplate.opsForValue()
+        val findBitSet: BitSet? = redis.get(key)
 
-        when (bitSet.isExists) {
-            true -> bitSet.set(userId, true)
-            else -> bitSet.set(BitSet(MAX_ACCOUNTS_NUMBER))
+        when (findBitSet?.isEmpty) {
+            false -> findBitSet.set(userId.toInt(), true)
+            else -> {
+                val bitSet = BitSet(MAX_ACCOUNTS_NUMBER)
+                bitSet.set(userId.toInt(), true)
+                redis.set(key, bitSet)
+            }
         }
     }
 

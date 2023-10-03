9edb2681acf24afd128c5551956b797e54a873c2
jihoon <znftm97@naver.com>
2023-10-04 00:59:44 +0900

    [#17] config : bulk insert를 위한 설정 추가, sql 파일 수정

--- coupon-platform-interfaces/src/test/resources/sql/data.sql
+++ coupon-platform-interfaces/src/test/resources/sql/data.sql
@@ -2,7 +2,7 @@ insert into account (external_id)
 values ('abcdedfghj11');
 
 insert into coupon (external_id, name, discount_type, price)
-values ('abcdedfghj23', 'coupon1', 'PRICE'. 1000);
+values ('abcdedfghj23', 'coupon1', 'PRICE', 1000);
 
 insert into coupon_code (external_id, coupon_id, expiration_period)
 values ('abcdedfghj23', '1', '2030-12-31 09:00:00');

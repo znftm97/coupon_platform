da24c92a50d7cdceb1a5cdd28efae38baaae2c47
jihoon <znftm97@naver.com>
2023-10-19 00:03:56 +0900

    [#21] test : 테스트 데이터 추가

--- coupon-platform-infrastructure/src/test/resources/sql/data.sql
+++ coupon-platform-infrastructure/src/test/resources/sql/data.sql
@@ -7,5 +7,23 @@ values ('abcdedfghj23', 'coupon1', 'PRICE', 1000);
 insert into coupon_code (external_id, coupon_id, expiration_period)
 values ('abcdedfghj23', '1', '2030-12-31 09:00:00');
 
-insert into issued_coupon (external_id, account_id, coupon_id, expiration_period, is_used)
-values ('abcdedfghj23', '1', '1', '2030-12-31 09:00:00', false);
+insert into issued_coupon (external_id, account_id, coupon_id, expiration_period, is_used, created_at)
+values ('abcdedfghj23', '1', '1', '2030-01-01 09:00:00', false, '2030-01-01 09:00:00');
+
+insert into issued_coupon (external_id, account_id, coupon_id, expiration_period, is_used, created_at)
+values ('abcdedfghj23', '1', '1', '2030-01-02 09:00:00', false, '2030-01-01 09:00:00');
+
+insert into issued_coupon (external_id, account_id, coupon_id, expiration_period, is_used, created_at)
+values ('abcdedfghj23', '1', '1', '2030-01-03 09:00:00', false, '2030-01-01 09:00:00');
+
+insert into issued_coupon (external_id, account_id, coupon_id, expiration_period, is_used, created_at)
+values ('abcdedfghj23', '1', '1', '2030-01-04 09:00:00', true, '2030-01-01 09:00:00');
+
+insert into coupon_daily_stats (coupon_usage_rate, coupon_daily_stats_id, created_at, expired_coupon_count, issued_coupon_count, used_coupon_count, external_id, stats_date)
+values ('0', '1', '2023-10-15 00:00:00.000000', '0', '1', '0', 'asdasddsa1', '2023-10-15');
+
+insert into coupon_daily_stats (coupon_usage_rate, coupon_daily_stats_id, created_at, expired_coupon_count, issued_coupon_count, used_coupon_count, external_id, stats_date)
+values ('0', '2', '2023-10-16 00:00:00.000000', '2', '10', '5', 'asdasddsa2', '2023-10-16');
+
+insert into coupon_daily_stats (coupon_usage_rate, coupon_daily_stats_id, created_at, expired_coupon_count, issued_coupon_count, used_coupon_count, external_id, stats_date)
+values ('100', '3', '2023-10-17 00:00:00.000000', '2', '10', '5', 'asdasddsa3', '2023-10-17');

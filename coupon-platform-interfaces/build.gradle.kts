f1561173160cf46510051cf613826d59ca3489c8
jihoon <znftm97@naver.com>
2023-10-18 23:19:41 +0900

    [#21] dependency : rest-assured 라이브러리 추가

--- coupon-platform-interfaces/build.gradle.kts
+++ coupon-platform-interfaces/build.gradle.kts
@@ -4,6 +4,8 @@ dependencies {
     implementation(project(":coupon-platform-infrastructure"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    testImplementation("io.rest-assured:kotlin-extensions:5.3.0")
 }
 
 tasks.register("prepareKotlinBuildScriptModel") {}

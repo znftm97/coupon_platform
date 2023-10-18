f09e0f48300d0a11825e0cd82cc7daeea558dc67
jihoon <znftm97@naver.com>
2023-10-19 00:02:25 +0900

    [#21] refactor : 쿠폰 조회,저장 단위 테스트 리팩토링

--- coupon-platform-domain/src/test/kotlin/coupon_platform/domain/coupon/CouponReadServiceUnitTest.kt
+++ coupon-platform-domain/src/test/kotlin/coupon_platform/domain/coupon/CouponReadServiceUnitTest.kt
@@ -1,7 +1,7 @@
 package coupon_platform.domain.coupon
 
-import coupon_platform.domain.coupon.dto.CouponInfo
 import coupon_platform.domain.coupon.entity.Coupon
+import coupon_platform.domain.coupon.repository.CouponReader
 import coupon_platform.domain.coupon.service.CouponReadService
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldBeEqual
@@ -10,47 +10,42 @@ import io.mockk.mockk
 
 class CouponReadServiceUnitTest : FunSpec({
 
-    val couponReadServiceMock = mockk<CouponReadService> {
-        every { couponReader } returns mockk()
-    }
+    val couponReader = mockk<CouponReader>()
+    val couponReadService = CouponReadService(couponReader)
 
     test("쿠폰 이름으로 쿠폰을 조회할 수 있다.") {
         val name = "name"
-        val couponInfo = CouponInfo(
-            1,
-            "externalId",
+        val coupon = Coupon.of(
             name,
             Coupon.DiscountType.PRICE,
             1000,
+            "externalId",
         )
-        every { couponReadServiceMock.findCouponByName(name) } returns couponInfo
-        val result = couponReadServiceMock.findCouponByName(name)
+        every { couponReader.findCouponByName(name) } returns coupon
+        val result = couponReadService.findCouponByName(name)
 
-        result.id shouldBeEqual 1
         result.externalId shouldBeEqual "externalId"
         result.name shouldBeEqual name
         result.discountType shouldBeEqual Coupon.DiscountType.PRICE
-        result.price shouldBeEqual couponInfo.price
+        result.price shouldBeEqual coupon.price
 
     }
 
     test("쿠폰id로 쿠폰을 조회할 수 있다.") {
         val couponId = 1L
-        val couponInfo = CouponInfo(
-            couponId,
-            "externalId",
+        val coupon = Coupon.of(
             "name",
             Coupon.DiscountType.PRICE,
             1000,
+            "externalId",
         )
-        every { couponReadServiceMock.findCouponById(couponId) } returns couponInfo
-        val result = couponReadServiceMock.findCouponById(couponId)
+        every { couponReader.findCouponById(couponId) } returns coupon
+        val result = couponReadService.findCouponById(couponId)
 
-        result.id shouldBeEqual couponId
         result.externalId shouldBeEqual "externalId"
         result.name shouldBeEqual "name"
         result.discountType shouldBeEqual Coupon.DiscountType.PRICE
-        result.price shouldBeEqual couponInfo.price
+        result.price shouldBeEqual coupon.price
     }
 
 })
--- coupon-platform-domain/src/test/kotlin/coupon_platform/domain/coupon/CouponStoreServiceUnitTest.kt
+++ coupon-platform-domain/src/test/kotlin/coupon_platform/domain/coupon/CouponStoreServiceUnitTest.kt
@@ -1,28 +1,41 @@
 package coupon_platform.domain.coupon
 
+import coupon_platform.domain.common.CommonConstants.EXTERNAL_ID_LENGTH
+import coupon_platform.domain.common.RandomNumberGenerator
 import coupon_platform.domain.coupon.entity.Coupon
+import coupon_platform.domain.coupon.repository.CouponStore
 import coupon_platform.domain.coupon.service.CouponStoreService
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldBeEqual
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 
 class CouponStoreServiceUnitTest : FunSpec({
 
-    val couponStoreServiceMock = mockk<CouponStoreService> {
-        every { couponStore } returns mockk()
-        every { randomNumberGenerator } returns mockk()
-    }
+    val couponStore = mockk<CouponStore>()
+    val randomNumberGenerator = mockk<RandomNumberGenerator>()
+    val couponCreateCommand = mockk<CouponCreateCommand>()
+    val couponStoreService = CouponStoreService(couponStore, randomNumberGenerator)
 
     test("쿠폰을 생성할 수 있다.") {
-        val name = "name"
-        val couponCreateCommand = CouponCreateCommand(
-            name,
+        val coupon = Coupon.of(
+            "name",
             Coupon.DiscountType.PRICE,
             1000,
+            "externalId",
         )
-        every {  couponStoreServiceMock.createCoupon(couponCreateCommand) } returns name
-        couponStoreServiceMock.createCoupon(couponCreateCommand) shouldBeEqual name
+
+        every {  couponStore.saveCoupon(coupon) } returns coupon
+        every {  couponCreateCommand.toEntity(coupon.externalId) } returns coupon
+        every {  randomNumberGenerator.generate(EXTERNAL_ID_LENGTH) } returns coupon.externalId
+
+        val couponName = couponStoreService.createCoupon(couponCreateCommand)
+
+        verify {  couponStore.saveCoupon(coupon) }
+        verify {  couponCreateCommand.toEntity(coupon.externalId) }
+        verify {  randomNumberGenerator.generate(EXTERNAL_ID_LENGTH) }
+        couponName shouldBeEqual coupon.name
     }
 
 })

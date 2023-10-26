9bf7d4314eba47272f29a4e0ea4fce297ea0c6bc
jihoon <znftm97@naver.com>
2023-10-26 16:16:22 +0900

    [#25] dependeny : time 관련 타입 직렬화, 역직렬화를 위한 jackson 모듈 추가

--- coupon-platform-infrastructure/build.gradle.kts
+++ coupon-platform-infrastructure/build.gradle.kts
@@ -7,6 +7,8 @@ dependencies {
     implementation("com.github.f4b6a3:tsid-creator:5.2.5")
     implementation("io.lettuce:lettuce-core:6.2.6.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-data-redis:3.1.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
 }
 
 tasks.register("prepareKotlinBuildScriptModel") {}

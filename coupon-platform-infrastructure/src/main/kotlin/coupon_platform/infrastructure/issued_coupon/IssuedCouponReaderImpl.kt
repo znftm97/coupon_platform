18a4b689ade2ee1863bc775bb8192e8a123810f5
jihoon <znftm97@naver.com>
2023-10-28 00:21:09 +0900

    [#25] feat : redis에서 발급된 쿠폰 조회하는 도메인 로직 추가

--- coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/issued_coupon/IssuedCouponReaderImpl.kt
+++ coupon-platform-infrastructure/src/main/kotlin/coupon_platform/infrastructure/issued_coupon/IssuedCouponReaderImpl.kt
@@ -2,15 +2,21 @@ package coupon_platform.infrastructure.issued_coupon
 
 import coupon_platform.domain.issued_coupon.entitiy.IssuedCoupon
 import coupon_platform.domain.issued_coupon.repository.IssuedCouponReader
+import coupon_platform.infrastructure.redis.handler.RedisHandlerOfIssuedCoupon
 import org.springframework.stereotype.Repository
-import java.time.ZonedDateTime
+import java.time.LocalDate
 
 @Repository
 class IssuedCouponReaderImpl(
     private val issuedCouponJpaRepository: IssuedCouponJpaRepository,
+    private val redisHandler: RedisHandlerOfIssuedCoupon,
 ): IssuedCouponReader {
 
-    override fun findIssuedCouponsInDates(dates: List<ZonedDateTime>): List<IssuedCoupon> {
+    override fun findIssuedCouponsInDates(dates: List<LocalDate>): List<IssuedCoupon> {
         return issuedCouponJpaRepository.findIssuedCouponsInDates(dates)
+    }
+
+    override fun findIssuedCouponsInDatesFromRedis(dates: List<LocalDate>): List<IssuedCoupon> {
+        return redisHandler.getIssuedCouponInDates(dates)
     }
 }

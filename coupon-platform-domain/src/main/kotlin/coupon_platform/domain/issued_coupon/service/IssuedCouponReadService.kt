77f2141956ecbbe7926cf59c2a3ae05a67dc9746
jihoon <znftm97@naver.com>
2023-10-28 00:22:35 +0900

    [#25] feat : 발급된 쿠폰 조회 시, Cache-Aside 패턴 적용

--- coupon-platform-domain/src/main/kotlin/coupon_platform/domain/issued_coupon/service/IssuedCouponReadService.kt
+++ coupon-platform-domain/src/main/kotlin/coupon_platform/domain/issued_coupon/service/IssuedCouponReadService.kt
@@ -3,23 +3,35 @@ package coupon_platform.domain.issued_coupon.service
 import coupon_platform.domain.issued_coupon.dto.IssuedCouponInfo
 import coupon_platform.domain.issued_coupon.entitiy.IssuedCoupon
 import coupon_platform.domain.issued_coupon.repository.IssuedCouponReader
+import coupon_platform.domain.issued_coupon.repository.IssuedCouponStore
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.ZoneId
-import java.time.ZonedDateTime
 
 @Service
 class IssuedCouponReadService(
     private val issuedCouponReader: IssuedCouponReader,
+    private val issuedCouponStore: IssuedCouponStore,
 ) {
 
-    fun findIssuedCouponsInDates(dates: List<LocalDate>): List<IssuedCouponInfo> {
-        val convertedDates: List<ZonedDateTime> = dates.map {
-            it.atStartOfDay(ZoneId.of("UTC"))
+    fun findIssuedCouponsInDates(reqDates: List<LocalDate>): List<IssuedCouponInfo> {
+        val issuedCouponsFromRedis: List<IssuedCoupon> = issuedCouponReader.findIssuedCouponsInDatesFromRedis(reqDates)
+        val emptyStatsDates: List<LocalDate> = getEmptyStatsDates(reqDates.toSet(), issuedCouponsFromRedis)
+
+        if(emptyStatsDates.isEmpty()) {
+            return issuedCouponsFromRedis.map { IssuedCouponInfo.toInfo(it) }
         }
 
-        val issuedCoupons: List<IssuedCoupon> = issuedCouponReader.findIssuedCouponsInDates(convertedDates)
+        val issuedCoupons: List<IssuedCoupon> = issuedCouponReader.findIssuedCouponsInDates(emptyStatsDates)
+        issuedCouponStore.saveIssuedCouponFromRedis(issuedCoupons)
+        issuedCoupons.toMutableList().addAll(issuedCouponsFromRedis)
+
         return issuedCoupons.map { IssuedCouponInfo.toInfo(it) }
     }
+
+    private fun getEmptyStatsDates(
+        reqDates: Set<LocalDate>,
+        issuedCouponsFromRedis: List<IssuedCoupon>,
+    ): List<LocalDate> = issuedCouponsFromRedis.map { it.createdAt.toLocalDate() }
+        .filterNot { reqDates.contains(it) }
 
 }
